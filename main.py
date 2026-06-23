9955a98d0d3a90e195adf9d7c7c98fbf8c94fe1c
patidarmithil <mithilpatidar80@gmail.com>
2026-06-23 20:44:49 +0530

    bug

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+# Hack for Streamlit Cloud to prevent libgthread errors
+# Uninstall GUI-dependent OpenCV that gets forced by ultralytics
+os.system(f"{sys.executable} -m pip uninstall -y opencv-python opencv-contrib-python")
+
 if "streamlit" in sys.modules:
     with open("dashboard/dashboard.py", "r", encoding="utf-8") as f:
         code = compile(f.read(), "dashboard/dashboard.py", 'exec')
